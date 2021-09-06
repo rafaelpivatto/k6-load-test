11771a0025dfd98db5044d185030c91616dd4d1e
Rafael Pivatto <rafael.pivatto@dextra-sw.com>
2021-09-06 00:15:30 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## K6 load test 
+#
+
+> https://k6.io/docs/
+
+```
+k6 run quarkus.js 
+```
+
+or
+
+```
+k6 run spring.js 
+```
---
+++ spring.js
@@ -0,0 +1,21 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+export let options = {
+    vus: 1000,
+    duration: "10s",
+    thresholds: {
+      http_req_duration: ['p(99)<2000'], // 99% of requests must complete below 2.0s
+    },
+  };
+export default function() {
+   
+    let res = http.get("http://localhost:9090/people?status=ATIVO");
+
+    if (res.status !== 200) {
+        console.log(`Status: ${res.status} - Duration: ${res.timings.duration}`)
+    }
+
+    check(res, {
+        "success": (r) => r.status == 200
+    });
+};
